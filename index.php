f18cca5090b7a39d97159cc01728c8bd0b1977ff
Yassineejjari <100588136+Yassineejjari@users.noreply.github.com>
2022-03-04 16:16:23 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
 ?>
 
 <main>
-<div><img src="https://2kdb.net/storage/players/2k22/giannis_antetokounmpo_52768.jpg" alt="" /></div>
-        <div><img src="https://2kdb.net/storage/players/2k22/allen_iverson_8979.jpg" alt="" /></div>
-        <div><img src="https://2kdb.net/storage/players/2k22/lamelo_ball_9202.jpg" alt="" /></div>
-        <div class="narrow"><img src="https://2kdb.net/storage/players/2k22/stephen_curry_9061.jpg" alt="2015" /></div>
-        <div><img src="https://2kdb.net/storage/players/2k22/damian_lillard_8603.jpg" alt="2014" /></div>
-        <div class="narrow"><img src="https://2kdb.net/storage/players/2k22/jordan_clarkson_8892.jpg" alt="2013" /></div>
+    <h1>Voici quelques maillots issues du jeu nba 2k22</h1>
+<div id="images">
+    <img src="https://2kdb.net/storage/players/2k22/giannis_antetokounmpo_52768.jpg" alt="" />
+        <img src="https://2kdb.net/storage/players/2k22/allen_iverson_8979.jpg" alt="" />
+        <img src="https://2kdb.net/storage/players/2k22/lamelo_ball_9202.jpg" alt="" />
+        <img src="https://2kdb.net/storage/players/2k22/stephen_curry_9061.jpg" alt="2015" />
+        <img src="https://2kdb.net/storage/players/2k22/jordan_clarkson_8892.jpg" alt="2013" />
+        <img src="https://2kdb.net/storage/players/2k22/tracy_mcgrady_8901.jpg" alt="2013" />
+    </div>
         
 
 </main>
