1da66969d2d7bbc45ebc5526184555f600140ec0
Yassineejjari <100588136+Yassineejjari@users.noreply.github.com>
2022-03-04 15:45:08 +0100

    Add files via upload

---
+++ form_recherche.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'debut_html.inc.php';
+
+?>
+
+
+<main>
+<div class="bar">
+   <div class="recherche">
+      <input type="text" class="rechercheTerm" placeholder="Quel maillot cherchez vous?">
+      <button type="submit" class="rechercheBouton">
+     </button>
+   </div>
+</div>
+</main>
+
+
+<?php
+
+    require 'fin_html.inc.php';
+
+?>
